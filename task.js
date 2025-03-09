855e5fdf2981945fc730ba638387675d267072b7
Hamsa <Hamsa2002alaa@gmail.com>
2025-03-09 02:42:29 +0200

    i did it

---
+++ task.js
@@ -0,0 +1,37 @@
+//let can change value
+let num = 6;
+num = 8; 
+
+const name = "Ahmed";
+name ="yousif"; // error , const not change
+
+var age = 12;
+age = 30; //can change value 
+
+
+//Arithmetic operations
+let a = 20;
+let b = 10;
+let sum = a + b; // sum=30
+let dif = a - b; // dif=10
+let product = a * b; // product=200
+let quotient = a / b; // quotient=2
+
+
+//logical operations
+let num1 = 1; // true
+let num2 = 0; //false
+let result = num1 && num2 // && =>And , result= false
+let result2 = num1 || num2 // || =>Or , result2= true
+
+let notnum1 = !num1 //false
+let notnum2 = !num2 //true
+
+
+//num to string
+let Num = 3;
+let stringNumber = Num.toString();
+
+//string to num 
+let stringNum = "6";
+let convertedNumber = Num(stringNum);
